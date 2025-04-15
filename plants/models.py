0d62605494918bd84582dd99626f38c80af86b2f
Sam Bowen <samhbowen@gmail.com>
2025-04-14 18:01:57 -0700

    create model

--- plants/models.py
+++ plants/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    description = models.TextField()
+    date_planted = models.DateField()
+
+    def __str__(self):
+        return self.name
