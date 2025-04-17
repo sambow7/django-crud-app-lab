19578e6d6e9ffddab8d9b0e430d84199adbbf9cd
Sam Bowen <samhbowen@gmail.com>
2025-04-17 09:45:02 -0700

    Adds basic plant catalog functionality
    Implements initial views for browsing a plant catalog.
    Includes list and detail views, as well as create, update,
    and delete views for managing plant records.

---
+++ plants/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from .views import PlantCreate, PlantUpdate, PlantDelete
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('plants/', views.plants_index, name='index'),
+    path('plants/<int:plant_id>/', views.plants_detail, name='detail'),
+    path('plants/create/', PlantCreate.as_view(), name='plants_create'),
+    path('plants/<int:pk>/update/', PlantUpdate.as_view(), name='plants_update'),
+    path('plants/<int:pk>/delete/', PlantDelete.as_view(), name='plants_delete'),
+]
--- plants/views.py
+++ plants/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Plant
 
-# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+
+def plants_index(request):
+    plants = Plant.objects.all()
+    return render(request, "plants/index.html", {"plants": plants})
+
+
+def plants_detail(request, plant_id):
+    plant = Plant.objects.get(id=plant_id)
+    return render(request, "plants/detail.html", {"plant": plant})
+
+class PlantCreate(CreateView):
+    model = Plant
+    fields = '__all__'
+    success_url = reverse_lazy('index')
+    template_name = 'plants/plant_form.html'
+
+class PlantUpdate(UpdateView):
+    model = Plant
+    fields = '__all__'
+    success_url = reverse_lazy('index')
+    template_name = 'plants/plant_form.html'
+
+class PlantDelete(DeleteView):
+    model = Plant
+    success_url = reverse_lazy('index')
+    template_name = 'plants/plant_confirm_delete.html'
